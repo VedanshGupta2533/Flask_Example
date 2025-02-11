9b63147fe5be49be07ebcdec023142949e4e298f
VedanshGupta2533 <guptavp@rknec.edu>
2025-02-11 17:01:44 +0530

    Adding the Code for ROUTES

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 @app.route("/")
 def hello_world():
- return "<p>Hello, World this flask</p>"
+    return "<p>Hello, World this flask</p>"
+
+@app.route("/products")
+def products():
+    return "<p>This is products page</p>"
+
 if __name__ == "__main__":
- app.run(debug=True)
+    app.run(debug=True)
